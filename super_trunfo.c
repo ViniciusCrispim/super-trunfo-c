305ec7c402f07fd518a2bf7c446a64e10becb3da
Vinícius Crispim <viniciuswoliveira@outlook.com>
2025-06-10 20:05:48 -0300

    Inicialização das variáveis e Entrada de dados Carta 1

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Definição da estrutura da carta
 struct Carta
 {
     char estado;
@@ -11,4 +12,33 @@ struct Carta
     int numPontosTuristicos;
 };
 
+int main () {
+    //Inicialização das variáveis das Cartas
+    struct Carta carta1, carta2;
 
+    //Leitura dos dados da Carta 1
+    printf("Digite os dados da Carta 1\n");
+    printf("Digite o Estado: ");
+    scanf(" %c", &carta1.estado);
+
+    printf("Digite o código da Carta 1: ");
+    scanf(" %s", carta1.codigo);
+
+    printf("Digite o nome da Cidade: ");
+    scanf(" %s", carta1.nomeCidade);
+
+    printf("Digite a população da Cidade: ");
+    scanf(" %d", &carta1.populacao);
+
+    printf("Digite o PIB da Cidade: ");
+    scanf(" %f", &carta1.pib);
+
+    printf("Digite a Área da Cidade: ");
+    scanf(" %f", &carta1.area);
+
+    printf("Digite o número de pontos turísticos: ");
+    scanf(" %d", &carta1.numPontosTuristicos);
+
+
+    return 0;
+};
