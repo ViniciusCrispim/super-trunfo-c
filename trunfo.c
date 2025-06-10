153669c60105767a14ac69000c765b4fd3983ee0
Vinícius Crispim <viniciuswoliveira@outlook.com>
2025-06-10 19:00:32 -0300

    Criação da Struct da carta

---
+++ trunfo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+struct Carta
+{
+    char estado;
+    char codigo[4];
+    char nomeCidade[100];
+    int populacao;
+    float area;
+    float pib;
+    int numPontosTuristicos;
+};
+
+
